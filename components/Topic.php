67a9b7900a629281da2876b2e6a57e785423ca81
Sam Georges <sam@daftspunk.com>
2014-04-17 18:06:42 +1000

    Basic topic creation

--- components/Topic.php
+++ components/Topic.php
@@ -1,9 +1,20 @@
 <?php namespace RainLab\Forum\Components;
 
+use DB as Db;
+use Flash;
+use Redirect;
 use Cms\Classes\ComponentBase;
+use RainLab\Forum\Models\Topic as TopicModel;
+use RainLab\Forum\Models\Channel;
+use RainLab\Forum\Models\Post;
 
 class Topic extends ComponentBase
 {
+
+    private $topic = null;
+    private $channel = null;
+
+    const PARAM_SLUG = 'slug';
 
     public function componentDetails()
     {
@@ -15,7 +26,100 @@ class Topic extends ComponentBase
 
     public function defineProperties()
     {
-        return [];
+        return [
+            'memberPage' => [
+                'title'       => 'Member Page',
+                'description' => 'Page name to use for clicking on a member.',
+                'type'        => 'string' // @todo Page picker
+            ],
+            'channelPage' => [
+                'title'       => 'Channel Page',
+                'description' => 'Page name to use for clicking on a channel.',
+                'type'        => 'string' // @todo Page picker
+            ],
+
+        ];
+    }
+
+    public function onRun()
+    {
+        $this->page['channel'] = $this->getChannel();
+        $this->page['topic'] = $this->getTopic();
+        $this->preparePostList();
+    }
+
+    public function getTopic()
+    {
+        if ($this->topic !== null)
+            return $this->topic;
+
+        if (!$slug = $this->param(static::PARAM_SLUG))
+            return null;
+
+        return $this->topic = TopicModel::whereSlug($slug)->first();
+    }
+
+    public function getChannel()
+    {
+        if ($this->channel !== null)
+            return $this->channel;
+
+        if ($topic = $this->getTopic())
+            $channel = $topic->channel;
+
+        elseif ($channelId = post('channel'))
+            $channel = Channel::find($channelId);
+
+        else
+            $channel = null;
+
+        return $this->channel = $channel;
+    }
+
+    protected function preparePostList()
+    {
+        /*
+         * Load the page links
+         */
+        $links = [
+            'member' => $this->property('memberPage'),
+            'channel' => $this->property('channelPage'),
+        ];
+
+        $this->page['forumLink'] = $links;
+    }
+
+    public function onCreate()
+    {
+        try {
+
+            $topic = TopicModel::make();
+            $topic->subject = post('subject');
+            $topic->channel = $this->getChannel();
+
+            $post = Post::make();
+            $post->topic = $topic;
+            $post->content = post('content');
+
+            Db::transaction(function() use ($topic, $post) {
+                $topic->save();
+                $post->save();
+            });
+
+            Flash::success(post('flash', 'Topic created successfully!'));
+
+            /*
+             * Redirect to the intended page after successful update
+             */
+            $redirectUrl = post('redirect', $this->currentPageUrl([
+                'slug' => $topic->slug
+            ]));
+            return Redirect::to($redirectUrl);
+
+        }
+        catch (\Exception $ex) {
+            Flash::error($ex->getMessage());
+        }
     }
 
 }
