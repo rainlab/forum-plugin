6e61e5109ef03da0ab0cb2b80b105072871110c4
Sam Georges <sam@daftspunk.com>
2014-05-23 19:43:44 +1000

    Add hidden and moderated fields to channels

---
+++ updates/channels_add_hidden_and_moderated.php
@@ -0,0 +1,25 @@
+<?php namespace RainLab\Forum\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class ChannelsAddHiddenAndModerated extends Migration
+{
+
+    public function up()
+    {
+        Schema::table('rainlab_forum_channels', function($table)
+        {
+            $table->boolean('hidden')->default(0);
+            $table->boolean('moderated')->default(0);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('rainlab_forum_channels', function($table)
+        {
+            $table->dropColumn('hidden', 'moderated');
+        });
+    }
+}
