da122dde7d244f644808082d7a97090cca677c46
Samuel Georges <sam@daftspunk.com>
2015-02-14 12:27:29 +1100

    Tidy up, API fixes

--- assets/js/forum.js
+++ assets/js/forum.js
@@ -1,26 +1,29 @@
-function formatForumQuote(author, quote)
-{
-    quote = "**" + author + "** said:\n\n" + quote;
-    quote = quote.replace(/^/g, ">");
-    quote = quote.replace(/\n/g, "\n>");
 
-    return quote;
+function formatForumQuote(author, quote) {
+    quote = "**" + author + "** said:\n\n" + quote
+    quote = quote.replace(/^/g, ">")
+    quote = quote.replace(/\n/g, "\n>")
+
+    return quote
 }
 
 $(document).ready(function() {
-	$(".quote").click(function (e) {
-		var postId = $(this).closest('.forum-post').data('post-id');
-		$.request('onQuote', {
-			data: { id: postId },
-		    success: function(data)
-		    {
-			    var obj = $.parseJSON(data.result);
-		        var quoteBody = obj.content;
-		        var authorName = obj.author;
-		       	var quoteText = formatForumQuote(authorName, quoteBody);
+    $('.quote').click(function (e) {
 
-	          	 $('#topicContent').val($('#topicContent').val() + quoteText + '\n\n').focus();
-		    }
-		});
-	});
+        var postId = $(this).closest('.forum-post').data('post-id')
+
+        $.request('onQuote', {
+            data: { id: postId },
+            success: function(data) {
+                var obj = $.parseJSON(data.result),
+                    quoteBody = obj.content,
+                    authorName = obj.author,
+                    quoteText = formatForumQuote(authorName, quoteBody)
+
+                $('#topicContent')
+                    .val($('#topicContent').val() + quoteText + '\n\n')
+                    .focus()
+            }
+        })
+    })
 });
