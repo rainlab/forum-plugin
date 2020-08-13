1abd705ec0f362afeae5bd32aaba3663f02a5a6d
Web-VPF <61043464+Web-VPF@users.noreply.github.com>
2020-08-13 07:31:06 +0300

    Update components/Channels.php
    Co-authored-by: Ben Thomson <ben@abweb.com.au>

--- components/Channels.php
+++ components/Channels.php
@@ -75,7 +75,7 @@ class Channels extends ComponentBase
 
     public function onRun()
     {
-        if ($this->property('includeStyles')) {
+        if ($this->property('includeStyles', true)) {
             $this->addCss('assets/css/forum.css');
         }
 
