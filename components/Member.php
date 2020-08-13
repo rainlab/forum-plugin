179b36921323d380e60e9112e594f71803983ae3
Web-VPF <61043464+Web-VPF@users.noreply.github.com>
2020-08-13 07:31:17 +0300

    Update components/Member.php
    Co-authored-by: Ben Thomson <ben@abweb.com.au>

--- components/Member.php
+++ components/Member.php
@@ -96,7 +96,7 @@ class Member extends ComponentBase
 
     public function onRun()
     {
-        if ($this->property('includeStyles')) {
+        if ($this->property('includeStyles', true)) {
             $this->addCss('assets/css/forum.css');
         }
 
